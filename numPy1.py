f04ac68985642f2b69e667c64f8e1d3e172ab2f2
bels <isabelamiranda10@icloud.com>
2026-04-17 14:12:37 -0300

    Add files via upload

---
+++ numPy1.py
@@ -0,0 +1,92 @@
+#Exercício_1:
+import numpy as np
+
+a = np.arange(1, 11)
+print(a)              # [1 2 3 4 5 6 7 8 9 10]
+print(a.shape)        # (10,)
+print(a[::-2])        # [10 8 6 4 2]
+print(a[::-1])        # [10 9 8 7 6 5 4 3 2 1]
+print(a.sum())        # 55
+print(a.mean())       # 5.5
+print(a[a > 5])       # [6 7 8 9 10]
+print(a[a % 3 == 0])  # [3 6 9]
+
+#Exercício_2:
+import numpy as np
+
+notas = np.array([7.5, 6.0, 8.5, 7.0])
+
+#Média 
+print(notas.mean())
+
+#Maior e menor
+print(notas.max(), notas.min())
+
+#Acima da média
+print(len(notas[notas > notas.mean()]))
+
+#Arredondar
+print(np.round(notas))
+
+
+#Exercício_3:
+import numpy as np
+
+precos = np.array([19.90, 35.50, 42.00, 8.90, 120.00, 55.00])
+
+#Desconto
+novos = precos * 0.85
+print(novos)
+
+#Maior que R$30
+print(novos[novos > 30])
+
+#Total
+print(novos.sum())
+
+
+#Exercício_4:
+import numpy as np
+
+temperaturas = np.array([22, 25, 19, 30, 28, 21])
+
+media = temperaturas.mean()
+print("Media:", media)
+
+acima_media = temperaturas[temperaturas > media]
+print("Acima da media:", acima_media)
+
+fahrenheit = temperaturas * 9/5 + 32
+print("Em Fahrenheit:", fahrenheit)
+
+
+#Exercício_5:
+import numpy as np
+
+alturas = np.array([1.65, 1.70, 1.68, 1.72, 1.60])
+idades = np.array([16, 17, 18, 17, 16])
+
+print(alturas.mean(), alturas.std())
+print(idades.max(), idades.min())
+
+print(alturas[idades >= 17])
+
+pesos = np.array([55, 60, 58, 65, 52])
+imc = pesos / (alturas ** 2)
+print(imc)
+
+
+#Exercício_6:
+import numpy as np
+
+dados = np.random.randint(1, 7, size=1000)
+
+print(dados.mean())
+
+valores, contagens = np.unique(dados, return_counts=True)
+print(valores, contagens)
+
+print((dados == 6).sum() / 1000 * 100)
+
+for n in [10, 100, 1000, 100000]:
+    print(np.random.randint(1, 7, size=n).mean())
